80fad7f1ed64373e909fe475b012bab7a40d801f
JonAJ21 <jonajmail@gmail.com>
2023-01-12 14:12:38 +0300

    Deleted trash

--- sem1/lab13/13.c
+++ sem1/lab13/13.c
@@ -1,4 +1,4 @@
-#include <string.h>	// Lab 13
+#include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <ctype.h>
@@ -33,53 +33,6 @@ void set_generate(set *s, bool indicator(int)) {
  }
 }
 
-void set_erase(set *s, int c) {
- s->data[c / bits_per_elem] &= ~(1u << c % bits_per_elem);
-}
-
-bool set_in(const set *s, int c) {
- return (s->data[c/ bits_per_elem] & (1u << c % bits_per_elem)) != 0;
-}
-
-int set_size(const set *s) {
- int i, size = 0;
- for(i = 0; i != 1 << bits_per_char; i++) {
- if(set_in(s, i)) {
- size++;
- }
- }
- return size;
-}
-
-bool set_equal(const set *s1, const set *s2) {
- int i;
- for(i = 0; i != datalen; i++) {
- if(s1->data[i] != s2->data[i]) {
- return false;
- }
- }
- return true;
-}
-
-bool set_includes(const set *s1, const set *s2) {
- int i;
- for(i = 0; i != datalen; i++) {
- if((s1->data[i] | s2->data[i]) != s1->data[i]) {
- return false;
- }
- }
- return true;
-}
-
-set set_union(const set *s1, const set *s2) {
- int i;
- set result;
- for(i = 0; i != datalen; i++) {
- result.data[i] = s1->data[i] | s2->data[i];
- }
- return result;
-}
-
 set set_intersection(const set *s1, const set *s2) {
  int i;
  set result;
@@ -89,31 +42,65 @@ set set_intersection(const set *s1, const set *s2) {
  return result;
 }
 
-set set_difference(const set *s1, const set *s2) {
- int i;
- set result;
- for(i = 0; i != datalen; i++) {
- result.data[i] = s1->data[i] & ~(s2->data[i]);
- }
- return result;
-}
-
-set set_symmetric_difference(const set *s1, const set *s2) {
- int i;
- set result;
- for(i = 0; i != datalen; i++) {
- result.data[i] = s1->data[i] ^ s2->data[i];
- }
- return result;
-}
-
 bool is_alpha(int c) { return isalpha(c); }
 bool is_digit(int c) { return isdigit(c); }
 
+// Является ли гласным
+bool is_vowel(int c) {
+    char vowels[] = "AEIOUaeiou";
+    if (strchr(vowels, c) != NULL && c != '\0') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// Является ли согласным
+bool is_consonant(int c) {
+    char consonants[] = "BCDFGJKLMNPQSTVXZHRWYbcdfgjklmnpqstvxzhrwy";
+    if (strchr(consonants, c) != NULL && c != '\0') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
+//Вариант 12
+// Есть ли гласная (согласная), входящая в состав всех слов?
 //--------------------------------------------
 int main()
 {
-    set s1, s2;
-    
+    set temp, vowels, consonants;
+    char symbol;
+    int flag = 1;
+    set_clear(&temp);
+    set_clear(&vowels);
+    set_clear(&consonants);
+    set_generate(&vowels, is_vowel);
+    set_generate(&consonants, is_consonant);
+    while (scanf("%c", &symbol) != EOF) {
+        if ((symbol == ' ' || symbol == ',' || symbol == '\n' || symbol == '\0' || symbol == '\t') && flag == 0) {
+            flag = 1;
+            vowels = set_intersection(&vowels, &temp);
+            consonants = set_intersection(&consonants, &temp);
+            set_clear(&temp);
+        } else if (is_alpha(symbol) != 0) {
+            flag = 0;
+            set_insert(&temp, symbol);
+        }
+    }
+    // Гласные
+    if (set_size(&vowels) > 0) {
+        printf("Vovels: yes, %d\n", set_size(&vowels));
+    } else {
+        printf("Vovels: no\n");
+    }
+    // Согласные
+    if (set_size(&consonants) > 0) {
+        printf("Consonants: yes, %d\n", set_size(&consonants));
+    } else {
+        printf("Consonants: no\n");
+    }
     return 0;
 }
